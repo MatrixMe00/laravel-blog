ca483460b1bc7591fe68aebb2f52143acdea3bbe
MatrixMe00 <safosah00@gmail.com>
2023-07-24 10:25:23 +0000

    Adding Categories to post and showing posts in a category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,7 @@
     <x-slot name="content">
         <article>
             <h1>{{ $post->title }}</h1>
-            <h5>Cat: <a href="#">{{$post->category->name}}</a></h5>
+            <h5>Cat: <a href="categories/{{$post->category->id}}">{{$post->category->name}}</a></h5>
             {!! $post->body !!}
         </article>
 
