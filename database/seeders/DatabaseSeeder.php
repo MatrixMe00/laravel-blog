b0ce405c2cce133bb5093b07f13471fed6808328
MatrixMe00 <safosah00@gmail.com>
2023-07-26 16:45:17 +0000

    Creating factories
    In this factory, the user is specifically defined

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,56 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //truncate tables to prevent repetition
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        //create a user
-        $user = User::factory()->create();
-
-        //create categories
-        $personal = Category::create([
-            "name" => "Personal",
-            "slug" => "personal"
+        $user = User::factory()->create([
+            "name" => "John Doe"
         ]);
 
-        $work = Category::create([
-            "name" => "Work",
-            "slug" => "work"
-        ]);
-
-        $hobby = Category::create([
-            "name" => "Hobby",
-            "slug" => "hobby"
-        ]);
-
-        //create posts
-        Post::create([
-            "user_id"=>$user->id,
-            "title"=>"My Personal Post",
-            "slug"=>"my-personal-post",
-            "category_id"=>$personal->id,
-            "excerpt"=>"<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>",
-            "body"=>"<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi a, iure et eum magni officia illum voluptates autem est facere dolores rerum eligendi qui blanditiis similique reiciendis? Aut, vel cumque.</p>"
-        ]);
-
-        Post::create([
-            "user_id"=>$user->id,
-            "title"=>"My Work Post",
-            "slug"=>"my-work-post",
-            "category_id"=>$work->id,
-            "excerpt"=>"<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>",
-            "body"=>"<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi a, iure et eum magni officia illum voluptates autem est facere dolores rerum eligendi qui blanditiis similique reiciendis? Aut, vel cumque.</p>"
-        ]);
-
-        Post::create([
-            "user_id"=>$user->id,
-            "title"=>"My Hobby Post",
-            "slug"=>"my-hobby-post",
-            "category_id"=>$hobby->id,
-            "excerpt"=>"<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>",
-            "body"=>"<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi a, iure et eum magni officia illum voluptates autem est facere dolores rerum eligendi qui blanditiis similique reiciendis? Aut, vel cumque.</p>"
+        Post::factory(5)->create([
+            "user_id" => $user->id
         ]);
 
         // \App\Models\User::factory()->create([
